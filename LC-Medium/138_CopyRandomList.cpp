12b76fa2d9473bba7e4ebfe61561e9ed974471df
badmuffin <itsabhijeet06@gmail.com>
2026-02-27 23:58:46 +0530

    feat: lc 138 copy random list new approach

--- LC-Medium/138_CopyRandomList.cpp
+++ LC-Medium/138_CopyRandomList.cpp
@@ -26,7 +26,7 @@ public:
 
 class Solution {
 public:
-    // using two pass approach with hashmap
+    // Approach 1:- using two pass approach with hashmap
     Node* copyRandomList(Node* head) {
         unordered_map<Node*, Node*> nodeMapper;
         // edge case, when random pointer is pointing to nullptr
@@ -50,5 +50,43 @@ public:
         }
 
         return nodeMapper[head];
+    }
+
+    // Approach 2:- without using extra space
+    Node* copyRandomListNew(Node* head) {
+        // step 1: copy the node and insert them in-between
+        Node* curr = head;
+        while (curr != nullptr) {
+            Node* copy = new Node(curr->val);
+            copy->next = curr->next;
+            curr->next = copy;
+            curr = curr->next->next;
+        }
+
+        // step 2: connect the random pointers
+        curr = head;
+        while (curr != nullptr) {
+            Node* copy = curr->next;
+
+            if (curr->random != nullptr)
+                copy->random = curr->random->next;
+            else
+                copy->random = nullptr;
+    
+            curr = curr->next->next;
+        }
+
+        // step 3: connect the next pointers
+        Node* dummy = new Node(-1);
+        Node* temp = dummy;
+        curr = head;
+        while (curr != nullptr) {
+            temp->next = curr->next;
+            curr->next = curr->next->next;
+            temp = temp->next;
+            curr = curr->next;
+        }
+
+        return dummy->next;
     }
 };
